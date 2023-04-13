f2e31adb5c47d9d72f6a24701103dcf7c8b53fb8
Oscar <of5162@gmail.com>
2023-04-13 16:59:15 -0600

    creación de la clase interfaz y model de semestres y grupo

---
+++ src/main/java/com/unsis/odonto/edu/modell/IGrupoModel.java
@@ -0,0 +1,19 @@
+/**
+ * Autor: Oscar Funtes Alvarado
+ * Fecha creación: 13 de abril de 2023
+ * Fecha modificación: 13 de abril de 2023
+ * Descripción: interfaz para modelar los métodos abstractos de la clase model
+ *              referente a grupo
+ */
+package com.unsis.odonto.edu.modell;
+
+import com.unsis.odonto.edu.entity.Grupo;
+import java.util.List;
+
+public interface IGrupoModel {
+    public void crearRegistro(Grupo grupo);
+    public List<Grupo> obtenerRegistros();
+    public void eliminarRegistro(Grupo grupo);
+    public Grupo obtenerRegistro(int idGrupo);
+    public void actualizarRegistro(Grupo grupo);
+}
