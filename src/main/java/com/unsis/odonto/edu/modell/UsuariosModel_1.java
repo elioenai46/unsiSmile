a586c0a976e77eb30ffe8a819f96214c3773dbf9
Oscar <of5162@gmail.com>
2023-04-14 08:32:16 -0600

    creación de la clase interfaz y model de administradores

---
+++ src/main/java/com/unsis/odonto/edu/modell/UsuariosModel_1.java
@@ -0,0 +1,101 @@
+/**
+ * Autor: Oscar Funtes Alvarado
+ * Fecha creación: 13 de abril de 2023
+ * Fecha modificación: 13 de abril de 2023
+ * Descripción: clase model para administradores, modelamos el crud de dicho objeto
+ *              implementando una clase interfaz
+ */
+
+package com.unsis.odonto.edu.modell;
+
+import com.unsis.odonto.edu.entity.Administradores;
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class AdministradoresModel_1 implements IAdministradoresModel {
+
+    private SessionFactory sf;
+    private Session s;
+
+    @Override
+    public void crearRegistro(Administradores administradores) {
+        try {
+            sf = new Configuration().configure().buildSessionFactory();
+            s = sf.openSession();
+            s.beginTransaction();
+            s.save(administradores);
+            //s.createNamedQuery("guardar()");
+            //s.
+            s.getTransaction().commit();
+            s.close();
+            sf.close();
+        } catch (HibernateException e) {
+            System.out.println("Error al crear el registro: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public List<Administradores> obtenerRegistros() {
+        List<Administradores> listaEjemplareses = new ArrayList<>();
+        try {
+            sf = new Configuration().configure().buildSessionFactory();
+            s = sf.openSession();
+            listaEjemplareses = s.createCriteria(Administradores.class).list();
+            s.close();
+            sf.close();
+        } catch (HibernateException e) {
+            System.out.println("Error al crear el registro: " + e.getMessage());
+        }
+        return listaEjemplareses;
+    }
+
+    @Override
+    public void eliminarRegistro(Administradores administradores) {
+        try {
+            sf = new Configuration().configure().buildSessionFactory();
+            s = sf.openSession();
+            s.beginTransaction();
+            s.delete(administradores);
+            s.getTransaction().commit();
+            s.close();
+            sf.close();
+        } catch (HibernateException e) {
+            System.out.println("Error al crear el registro: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public Administradores obtenerRegistro(int idAdministrador) {
+        Administradores administradores = null;
+        try {
+            sf = new Configuration().configure().buildSessionFactory();
+            s = sf.openSession();
+            administradores = s.get(Administradores.class, idAdministrador);
+            s.close();
+            sf.close();
+        } catch (HibernateException e) {
+            System.out.println("Error al crear el registro: " + e.getMessage());
+        }
+        return administradores;
+    }
+
+    @Override
+    public void actualizarRegistro(Administradores administradores) {
+        try {
+            sf = new Configuration().configure().buildSessionFactory();
+            s = sf.openSession();
+            s.beginTransaction();
+            s.update(administradores);
+            s.getTransaction().commit();
+            s.close();
+            sf.close();
+        } catch (HibernateException e) {
+            System.out.println("Error al crear el registro: " + e.getMessage());
+        }
+    }
+
+}
