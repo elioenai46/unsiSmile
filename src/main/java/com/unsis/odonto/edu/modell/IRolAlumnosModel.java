5f4d00e37a0be0008888df32cc5c18919d4c9fc5
Niky11 <nancyobed11@gmail.com>
2023-04-13 23:44:15 -0600

    creando la interfaz de IRolAlumno

---
+++ src/main/java/com/unsis/odonto/edu/modell/IRolAlumnosModel.java
@@ -0,0 +1,20 @@
+/**
+ *Autor:Nancy Obed Martínez Miguel
+ *Fecha de creación:13 de abril 2023
+ *Fecha de Modificación:
+ *Descripción:
+ */
+package com.unsis.odonto.edu.modell;
+
+
+import com.unsis.odonto.edu.entity.RolAlumno;
+import java.util.List;
+
+
+public interface IRolAlumnosModel {
+    public void crearRegistro(RolAlumno rolAlumno);
+    public List<RolAlumno> obtenerRegistros();
+    public void eliminarRegistro(RolAlumno rolAlumno);
+    public RolAlumno obtenerRegistro(int id);
+    public void actualizarRegistro(RolAlumno rolAlumno);
+}
