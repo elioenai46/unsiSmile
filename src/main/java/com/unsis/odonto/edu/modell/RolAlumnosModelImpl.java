11aad0a73aaf37ced717a1d3d8f483de334496e5
Niky11 <nancyobed11@gmail.com>
2023-04-13 23:54:02 -0600

    creando la clase de RolAlumnoModelImpl

---
+++ src/main/java/com/unsis/odonto/edu/modell/RolAlumnosModelImpl.java
@@ -0,0 +1,99 @@
+/**
+ *Autor:Nancy Obed Martínez Miguel
+ *Fecha de creación:13 de abril 2023
+ *Fecha de Modificación:
+ *Descripción:
+ */
+package com.unsis.odonto.edu.modell;
+
+import com.unsis.odonto.edu.entity.RolAlumno;
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ *
+ * @author TECNOCITY
+ */
+public class RolAlumnosModelImpl implements IRolAlumnosModel {
+    private SessionFactory sf;
+    private Session s;
+    @Override
+    public void crearRegistro(RolAlumno rolAlumno) {
+         try {
+            sf = new Configuration().configure().buildSessionFactory();
+            s = sf.openSession();
+            s.beginTransaction();
+            s.save(rolAlumno);
+            s.getTransaction().commit();
+            s.close();
+            sf.close();
+        } catch (HibernateException e) {
+            System.out.println("Error al crear el registro: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public List<RolAlumno> obtenerRegistros() {
+         List<RolAlumno> listaRolAlumno = new ArrayList<>();
+        try {
+            sf = new Configuration().configure().buildSessionFactory();
+            s = sf.openSession();
+            listaRolAlumno = s.createCriteria(RolAlumno.class).list();
+            s.close();
+            sf.close();
+        } catch (HibernateException e) {
+            System.out.println("Error al crear el registro: " + e.getMessage());
+        }
+        return listaRolAlumno;
+    }
+
+    @Override
+    public void eliminarRegistro(RolAlumno rolAlumno) {
+        try {
+            sf = new Configuration().configure().buildSessionFactory();
+            s = sf.openSession();
+            s.beginTransaction();
+            s.delete(rolAlumno);
+            s.getTransaction().commit();
+            s.close();
+            sf.close();
+        } catch (HibernateException e) {
+            System.out.println("Error al crear el registro: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public RolAlumno obtenerRegistro(int id) {
+        RolAlumno rolAlumno = null;
+        try {
+            sf = new Configuration().configure().buildSessionFactory();
+            s = sf.openSession();
+            rolAlumno = s.get(RolAlumno.class, id);
+            s.close();
+            sf.close();
+        } catch (HibernateException e) {
+            System.out.println("Error al crear el registro: " + e.getMessage());
+        }
+        return rolAlumno;
+    }
+
+    @Override
+    public void actualizarRegistro(RolAlumno rolAlumno) {
+         try {
+            sf = new Configuration().configure().buildSessionFactory();
+            s = sf.openSession();
+            s.beginTransaction();
+            s.update(rolAlumno);
+            s.getTransaction().commit();
+            s.close();
+            sf.close();
+        } catch (HibernateException e) {
+            System.out.println("Error al crear el registro: " + e.getMessage());
+        }
+    }
+    
+}
