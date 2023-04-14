112884015781201264fd0d77a287d409fea2d5c4
Niky11 <nancyobed11@gmail.com>
2023-04-13 19:55:17 -0600

    creando la clase ClinicaModelImpl

--- src/main/java/com/unsis/odonto/edu/modell/IClinicaModel.java
+++ src/main/java/com/unsis/odonto/edu/modell/IClinicaModel.java
@@ -6,14 +6,18 @@
  */
 package com.unsis.odonto.edu.modell;
 
-
 import com.unsis.odonto.edu.entity.Clinica;
 import java.util.List;
 
 public interface IClinicaModel {
-     public void crearRegistro(Clinica clinica);
+
+    public void crearRegistro(Clinica clinica);
+
     public List<Clinica> obtenerRegistros();
+
     public void eliminarRegistro(Clinica clinica);
+
     public Clinica obtenerRegistro(int id);
+
     public void actualizarRegistro(Clinica clinica);
 }
