f3195883f53bd87c10a325832102f488b7f852fb
FernandoVite14 <jesush.vite@gmail.com>
2023-05-10 16:27:30 -0600

    Listar pacientes// Manejo de fechas corregido listado correcto. cambio al nombre de variables a petición del Mtro.Rolando

--- src/main/java/com/unsis/odonto/edu/modell/PacienteModelImpl.java
+++ src/main/java/com/unsis/odonto/edu/modell/PacienteModelImpl.java
@@ -58,33 +58,30 @@ public class PacienteModelImpl implements IPacienteModel {
             sp.execute();
             
             // Obtener los resultados de la consulta
-            List<Object[]> registroP =  sp.getResultList();
+            List<Object[]> listaRegistros =  sp.getResultList();
             
             // Recorrer los registros y agregarlos a la lista listapPacientes
-            for(Object[] rP : registroP){
+            for(Object[] registroPaciente : listaRegistros){
                 
-                //System.out.println("Datos: " + rP[1].toString());
-                //Object[] row = registroP.get(0);
-                p1.setIdPaciente((rP[0] == null) ? Integer.valueOf("1") : Integer.parseInt((rP[0]).toString()));
-                p1.setNombre1((rP[1] == null) ? "" : (rP[1]).toString());
-                p1.setNombre2((rP[2] == null) ? "" : (rP[2]).toString());
-                p1.setApellido1((rP[3] == null) ? "" : (rP[3]).toString());
-                p1.setApellido2((rP[4] == null) ? "" : (rP[4]).toString());
-                p1.setSexo((rP[5] == null) ? '\0' : (rP[5]).toString().charAt(0));
-                p1.setGrupoEtnico((rP[6] == null) ? "" : (rP[6]).toString());
-                p1.setOcupacion((rP[7] == null) ? "" : (rP[7]).toString());
-                System.out.println("fecha"+rP[8].toString());
-                date = rP[8].toString();
+                p1.setIdPaciente((registroPaciente[0] == null) ? Integer.valueOf("1") : Integer.parseInt((registroPaciente[0]).toString()));
+                p1.setNombre1((registroPaciente[1] == null) ? "" : (registroPaciente[1]).toString());
+                p1.setNombre2((registroPaciente[2] == null) ? "" : (registroPaciente[2]).toString());
+                p1.setApellido1((registroPaciente[3] == null) ? "" : (registroPaciente[3]).toString());
+                p1.setApellido2((registroPaciente[4] == null) ? "" : (registroPaciente[4]).toString());
+                p1.setSexo((registroPaciente[5] == null) ? '\0' : (registroPaciente[5]).toString().charAt(0));
+                p1.setGrupoEtnico((registroPaciente[6] == null) ? "" : (registroPaciente[6]).toString());
+                p1.setOcupacion((registroPaciente[7] == null) ? "" : (registroPaciente[7]).toString());
+                date = registroPaciente[8].toString();
                 LocalDate ld = LocalDate.parse(date);
-                p1.setFechaNacimiento((rP[8] == null) ? LocalDate.of(2000,05,15) : ld);
-                p1.setEstadoCivil((rP[10] == null) ? "" : (rP[10]).toString());
-                date2 = rP[12].toString();
+                p1.setFechaNacimiento((registroPaciente[8] == null) ? LocalDate.of(0,0,0) : ld);
+                p1.setEstadoCivil((registroPaciente[10] == null) ? "" : (registroPaciente[10]).toString());
+                date2 = registroPaciente[12].toString();
                 LocalDate ld2 = LocalDate.parse(date2);
-                p1.setFechaIngreso((rP[12] == null) ? LocalDate.of(0, Month.MARCH, 0) : ld2);
-                p1.setNacionalidad((rP[13] == null) ? "" : (rP[13]).toString());
-                p1.setLocalidad((rP[14] == null) ? "" : (rP[14]).toString());
-                boolean x = Boolean.parseBoolean((rP[15]).toString());
-                p1.setEstatus((rP[15] == null) ? true : x);
+                p1.setFechaIngreso((registroPaciente[12] == null) ? LocalDate.of(0, Month.MARCH, 0) : ld2);
+                p1.setNacionalidad((registroPaciente[13] == null) ? "" : (registroPaciente[13]).toString());
+                p1.setLocalidad((registroPaciente[14] == null) ? "" : (registroPaciente[14]).toString());
+                boolean x = Boolean.parseBoolean((registroPaciente[15]).toString());
+                p1.setEstatus((registroPaciente[15] == null) ? true : x);
                 listapPacientes.add(p1);
             }
             
