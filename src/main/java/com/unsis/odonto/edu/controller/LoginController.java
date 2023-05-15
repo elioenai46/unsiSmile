b9e540d2cca5c9472d323e0729827808536b1191
FernandoVite14 <jesush.vite@gmail.com>
2023-05-15 16:22:09 -0600

    Listado de pacientes por alumno completa

--- src/main/java/com/unsis/odonto/edu/controller/LoginController.java
+++ src/main/java/com/unsis/odonto/edu/controller/LoginController.java
@@ -45,6 +45,7 @@ public class LoginController extends HttpServlet {
            
             HttpSession session = request.getSession();
             session.setMaxInactiveInterval(20);
+            System.out.println("id--: " +usuarios.getIdUsuario());
             session.setAttribute("idUsuario", usuarios.getIdUsuario());
              switch (usuarios.getNombreRol()) {
                 case "Administrador":              
--- src/main/java/com/unsis/odonto/edu/controller/PacienteServletController.java
+++ src/main/java/com/unsis/odonto/edu/controller/PacienteServletController.java
@@ -1,7 +1,7 @@
 /**
  * Autor     : Vite 
  * Fecha de creación : 20 abr 2023, 16:05
- * Fecha de modificación:
+ * Fecha de modificación:12 may 2023, 16:55
  * Descripción : PacienteServletController de gestionar pacientes, enlace con Paciente. 
  */
  
@@ -73,15 +73,20 @@ public class PacienteServletController extends HttpServlet {
         dispatcher.forward(request, response);
     }
 
+    //Método para listar pacientes mediante la valuación del id de alumno
     private void listar(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {        
         System.out.println(
                 "---------------VITE------------------------------");
 
         RequestDispatcher dispatcher = request.getRequestDispatcher("/pages/viewAlumno/gestionarPacientes.jsp");
-
-        Integer id = Integer.valueOf(request.getParameter("idUsuario"));
+        
+        //obtener id
+        String idUsuario = request.getParameter("idUsuario");
+        Integer id = Integer.valueOf(idUsuario);
         System.out.println(id);
+        
+        //llamado al service
         IPacienteService service = new PacienteServiceImpl();
         List<Paciente> listaPaciente = service.obtenerRegistros(id);
 
