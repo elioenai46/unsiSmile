7ebc3af00c99ae7883b29feaec737defcdc931bf
froste <froste@fr0ste>
2023-05-18 10:09:58 -0600

    Agregado. Archivos js para hacer peticiones de eliminado

---
+++ src/main/webapp/resources/js/eliminarCatedratico.js
@@ -0,0 +1,7 @@
+/**
+ * Autor: Joel Francisco Figueroa Martinez
+ * Fecha de creación: 18 de mayo de 2023
+ * Fecha de actualización: 18 de mayo de 2023
+ * Descripción: archivo js para hacer la peticion para eliminar un catedratico
+ */
+
